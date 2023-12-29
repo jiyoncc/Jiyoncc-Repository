a4fe1f01487292257e832a58a24944be3f49cc23
John Carlo Corpus <146500016+jiyoncc@users.noreply.github.com>
2023-12-29 22:35:09 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,177 @@
-<!DOCTYPE html>
-<html>
+<?php
+include('include/db.php');
+
+session_start();
+if (!empty($_SESSION['cart'])) {
+    $printCount = count($_SESSION['cart']);
+}
+else {
+    $printCount = 0;
+}
+if (!empty($_SESSION['user_id']) && !empty($_SESSION['username'])) {
+    $printUsername = $_SESSION['username'];
+}
+else {
+    $printUsername = ""; 
+}
+?>
+<!doctype html>
+<html lang="en">
+ 
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <style>
-        .container {
-            max-width: 500px;
-            margin: 0 auto;
-            background-color: pink;
-            padding: 20px;
-            border-radius: 10px;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sweet & Delices Cake Shop</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/userpage.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
 </head>
+
 <body>
-    <div class="container">
-        <form action="login.php" method="post">
-            <h1>Welcome to our Page!</h1>
-            <h2>Please fill out the form.</h2>
-            <div class="mb-3">
-                <label for="fullname">Fullname:</label>
-                <input type="text" class="form-control" id="fullname" name="fullname">
+    <div class="dashboard-main-wrapper">
+        <?php
+        include("./include/nav.php")
+        ?>
+            <div class="container-fluid dashboard-content">    
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner rounded">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="uploads/1.jpg" alt="First slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">GOOD FOOD, GOOD LIFE</h3>
+                                        <p>Cakes are special, every celebration ends with something sweet like a cake and people remember it's all about the memories.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="uploads/2.jpg" alt="Second slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">The cake we bake with love</h3>
+                                        <p>Take the shredded pieces of your life and bake a master cake out of it.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="uploads/3.jpg" alt="Third slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">Love at first bite</h3>
+                                        <p>You can't be down when you are holding a cupcake.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="uploads/5.jpg" alt="Fourth slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">A party without cake is really just a meeting.</h3>
+                                        <p>Cakes are special, every celebration ends with something sweet like a cake and people remember it's all about the memories.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="uploads/6.jpg" alt="Fifth slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">Eat a cake.</h3>
+                                        <p>Cakes are special, every celebration ends with something sweet like a cake and people remember it's all about the memories.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="uploads/7.jpg" alt="Sixth slide">
+                                    <div class="carousel-caption d-md-block pb-5">
+                                        <h3 class="text-white">View our categories.</h3>
+                                        <p>Cakes are special, every celebration ends with something sweet like a cake and people remember it's all about the memories.</p>
+                                        <a href="about.php" class="btn btn-rounded btn-outline-light">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                         <span class="sr-only">Previous</span>   </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                   <span class="sr-only">Next</span>  </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row m-5">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">
+                        <h1>Our Features</h1>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                        <div class="card text-center p-3">
+                            <div class="card-body">
+                                <h1 class="card-title"><i class="fas fa-thumbs-up"></i></h1>
+                                <h3 class="card-title">Quality</h3>
+                                <p class="card-text">Our very first priority is the quality we never compromised in the quality of our bakery products.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                        <div class="card text-center p-3">
+                            <div class="card-body">
+                                <h1 class="card-title"><i class="fas fa-birthday-cake"></i></h1>
+                                <h3 class="card-title">Fresh & natural</h3>
+                                <p class="card-text">Our every product is fresh and made with natural ingredients we do not use the artificial food ingredient in our products.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                        <div class="card text-center p-3">
+                            <div class="card-body">
+                                <h1 class="card-title"><i class="fas fa-shipping-fast"></i></h1>
+                                <h3 class="card-title">Free delivery</h3>
+                                <p class="card-text">We provide free delivery to our customers. We deliver in 1 hr from the time customer order the product.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+                 include("./include/category_card.php")
+                ?>
+
+                <div class="row m-5 hero-image2 rounded">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 p-3 hero-text">
+                        <h1 class="text-dark">Who We Are</h1>
+                        <p class="text-dark px-5">We are bakers, we bake the piece of joy. We believe cake and baked goods are an expression of love.</p>
+                        <p class="text-dark px-5">We bake from scratch daily using traditional methods and quality ingredients. There are some things in life you just can't fake, and dang good cake? That's one of them. We use organic whole milk, cage-free eggs, loads of real fruit, pure extracts, amazingly delicious chocolate, and lots and lots of real butter to create simply delicious treats the old-fashioned way.</p>
+                        <a href="about.php" class="btn btn-rounded btn-success">Read More</a>
+                    </div>
+                </div>
+
+                <div class="row mx-5 hero-image rounded">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 p-3 hero-text">
+                        <h1 class="text-white">Always happy to hear from you.</h1>
+                        <a href="contact.php" class="btn btn-rounded btn-brand">Contact Us</a>
+                    </div>
+                </div>
+
             </div>
-            <div class="mb-3">
-                <label for="uname">Username:</label>
-                <input type="text" class="form-control" id="uname" name="uname">
-            </div>
-            <div class="mb-3">
-                <label for="pass1">Password:</label>
-                <input type="password" class="form-control" id="pass1" name="pass1">
-            </div>
-            <div class="mb-3">
-                <label for="pass2">Confirm Password:</label>
-                <input type="password" class="form-control" id="pass2" name="pass2">
-            </div>
-            <div class="mb-3">
-                <input type="submit" class="btn btn-success" value="Submit">
-            </div>
-            <div class="mb-3">
-                <br><a href="registration.php">Create Account</a>
-            </div>
-        </form>
+
+            <?php
+            include("./include/footer.php")
+            ?>
     </div>
-    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.slimscroll.js"></script>
+    <script src="js/main-js.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.owl-carousel').owlCarousel({
+                loop: true, margin: 10, dots: 0, autoplay: 4000, autoplayHoverPause: true, responsive:{
+                    0:{items:1}, 600:{items:2}, 1000:{items:4}
+                }
+            })
+        });
+    </script>
 </body>
 </html>
